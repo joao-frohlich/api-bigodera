a00cf4bc16e999d818b84afd9d91d99f9ed7f71a
Joao Vitor Frohlich <joaovitorfrohlich@gmail.com>
2021-07-06 22:54:38 -0300

    algumas coisas

--- Classes/Database.py
+++ Classes/Database.py
@@ -1,12 +1,14 @@
 import psycopg2
+import os
 
 class Database:
     def __init__(self):
         self.connection = psycopg2.connect(
-            host='localhost',
-            database='Bigodera',
-            user='postgres',
-            password='postgres'
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB_NAME'],
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASS'],
+            port=os.environ['DB_PORT']
         )
         self.cursor = self.connection.cursor()
 
--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export $(xargs < configure.env)
 export FLASK_APP=app
 export FLASK_ENV=development
 export FLASK_RUN_PORT=5001
