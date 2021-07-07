ea85c6a07494bf47c2a274a4ba75a9152b5f2dea
Joao Vitor Frohlich <joaovitorfrohlich@gmail.com>
2021-07-06 23:20:58 -0300

    foto_de_anime.jpg

--- Classes/AddMeme.py
+++ Classes/AddMeme.py
@@ -5,7 +5,7 @@ class AddMeme(Resource):
     def get(self, new_meme):
         db = Database()
         try:
-            db.insert('meme', 'cod_meme, texto_meme', '1, \'' + new_meme.replace('_', ' ') + '\'')
+            db.insert('meme', 'texto_meme', '\'' + new_meme.replace('_', ' ') + '\'')
             return {1: 'Meme inserido com sucesso'}
         except:
             return {1: 'Falha ao adicionar novo meme ;-;'}
